2488587950dfd1f261322b248762f8d4a5a747fc
Sjoerd Dost <deosjr@gmail.com>
2020-12-08 09:11:22 +0100

    day08 in prolog without asserts

--- 08/day08.pl
+++ 08/day08.pl
@@ -1,72 +1,78 @@
 :- ['../lib/io.pl'].
 
-:- dynamic instr/3.
+exec(nop, _, 1, 0).
+exec(acc, N, 1, N).
+exec(jmp, N, N, 0).
 
-exec("nop", _, 1, 0).
-exec("acc", N, 1, N).
-exec("jmp", N, N, 0).
-
-run_program(Ptr, OldAcc, Acc, Seen) :-
-    instr(Ptr, Instr, N),
-    exec(Instr, N, NPtr, NAcc),
+run_program(Program, Ptr, OldAcc, Acc, Seen) :-
+    [Op, N] = Program.Ptr,
+    exec(Op, N, NPtr, NAcc),
     NewPtr #= Ptr + NPtr,
     NewAcc #= OldAcc + NAcc,
     (
-        member(NewPtr, Seen)
+        intersection([NewPtr], Seen, [NewPtr])
     ->
         Acc #= NewAcc
     ;
         NewSeen = [NewPtr|Seen],
-        run_program(NewPtr, NewAcc, Acc, NewSeen)
+        run_program(Program, NewPtr, NewAcc, Acc, NewSeen)
     ).
 
-part1(Ans) :-
-    run_program(0, 0, Ans, []).
+part1(Program, Ans) :-
+    run_program(Program, 0, 0, Ans, []).
 
-mod("nop", "jmp").
-mod("jmp", "nop").
-mod("acc", "acc").
+mod(nop, jmp).
+mod(jmp, nop).
+mod(acc, acc).
 
-% this would be faster if we assert run state, but good enough for now
-run_program_modified(Mod, Ptr, OldAcc, Acc, Seen, Terminated) :-
-    instr(Ptr, Instr, N),
-    ( Ptr = Mod -> mod(Instr, ModInstr); Instr=ModInstr),
-    exec(ModInstr, N, NPtr, NAcc),
+run_program_modified(Program, Mod, Ptr, OldAcc, Acc, Seen, Terminated) :-
+    [Op, N] = Program.Ptr,
+    ( Ptr = Mod -> mod(Op, ModOp); Op=ModOp),
+    exec(ModOp, N, NPtr, NAcc),
     NewPtr #= Ptr + NPtr,
     NewAcc #= OldAcc + NAcc,
     (
-        member(NewPtr, Seen)
+        intersection([NewPtr], Seen, [NewPtr])
     ->
         Terminated = false,
         Acc #= NewAcc
     ;
         (
-            not(instr(NewPtr,_,_))
+            get_dict(NewPtr, Program, _)
         ->
+            NewSeen = [NewPtr|Seen],
+            run_program_modified(Program, Mod, NewPtr, NewAcc, Acc, NewSeen, Terminated)
+        ;
             Terminated = true,
             Acc #= NewAcc
-        ;
-            NewSeen = [NewPtr|Seen],
-            run_program_modified(Mod, NewPtr, NewAcc, Acc, NewSeen, Terminated)
         )
     ).
 
-part2(Ans) :-
-    findall(I, (instr(I, Instr, _), member(Instr, ["nop","jmp"])), List),
+nop_or_jmp(Program, I) :-
+    Program.I = [Op, _],
+    member(Op, [nop, jmp]).
+
+part2(Program, Ans) :-
+    numlist(0, Program.len, Nums),
+    include(nop_or_jmp(Program), Nums, List),
     maplist([X,Y]>>(
-        run_program_modified(X, 0, 0, Z, [], Terminated), Y=Z-Terminated
+        run_program_modified(Program, X, 0, 0, Z, [], Terminated), Y=Z-Terminated
     ), List, AnsList),
     member(Ans-true, AnsList).
 
-parse(LineNum) --> parse_line(LineNum), "\n", {NewLineNum #= LineNum+1}, parse(NewLineNum).
-parse(LineNum) --> parse_line(LineNum), blanks, eos.
+parse(LineNum, [Instr|T]) --> parse_line(LineNum, Instr), "\n",
+    {NewLineNum #= LineNum+1}, parse(NewLineNum, T).
+parse(LineNum, [Instr]) --> parse_line(LineNum, Instr), blanks, eos.
 
-parse_line(I) --> string_without(" ", C), " ", integer(N),
-    {string_codes(S, C), assertz(instr(I, S, N))}.
+parse_line(I,I-[S,N]) --> string_without(" ", C), " ", integer(N), {atom_codes(S, C)}.
 
 run :-
-    input_stream(8, parse(0)),
-    part1(Ans1),
+    input_stream(8, parse(0, List)),
+    dict_create(Dict, program, List),
+    dict_keys(Dict, [_|Keys1Based]),
+    length(Keys1Based, N),
+    Program = Dict.put(len, N),
+    part1(Program, Ans1),
     write_part1(Ans1),
-    part2(Ans2),
+    part2(Program, Ans2),
     write_part2(Ans2).
